ea53d3e2dbc81b588c3aabd63d268600ae469c1a
JayaniAththanagoda <165479877+JayaniAththanagoda@users.noreply.github.com>
2024-08-06 19:59:45 +0530

    image size change

--- src/componentss/Reviews.tsx
+++ src/componentss/Reviews.tsx
@@ -161,11 +161,10 @@ export function Reviews() {
       <img
         aria-hidden="true"
         src="/users/what-people-are-buying.png"
-        className="absolute w-28 left-0 -top-20  hidden lg:block"
+        className="absolute select-none hidden xl:block-left-32 top-1/3"
       />
 
       <ReviewGrid />
     </MaxWidthWrapper>
   );
 }
-/*absolute select-none hidden xl:block-left-32 top-1/3 */
